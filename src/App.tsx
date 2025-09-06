1953fb798c629535a573d9f70f5196fba5abbcc7
jjdavenport <jordanjamesdavenport@gmail.com>
2025-09-06 21:20:36 +0100

    add mobile layout, style

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,55 @@
+import { useState, useEffect } from "react";
+import {
+  Footer,
+  Wrapper,
+  Container,
+  Header,
+  Title,
+  Search,
+  Button,
+  Main,
+  List,
+  DailyList,
+  HourlyList,
+} from "./components/content";
+
 function App() {
-  return <></>;
+  const [lat, setLat] = useState(null);
+  const [long, setLong] = useState(null);
+
+  const fetchLatLong = async () => {
+    const response = await fetch("https://ipapi.co/json/");
+    const result = await response.json();
+    setLat(result.latitude);
+    setLong(result.longitude);
+  };
+
+  useEffect(() => {
+    fetchLatLong();
+  }, []);
+
+  useEffect(() => {
+    console.log(lat);
+    console.log(long);
+  }, [lat, long]);
+
+  return (
+    <>
+      <Wrapper>
+        <Container>
+          <Header />
+          <Title />
+          <Search />
+          <Button />
+          <Main />
+          <List />
+          <DailyList />
+          <HourlyList />
+        </Container>
+        <Footer />
+      </Wrapper>
+    </>
+  );
 }
 
 export default App;
--- src/components/content.tsx
+++ src/components/content.tsx
@@ -2,16 +2,49 @@ import logo from "../assets/images/logo.svg";
 import settings from "../assets/images/icon-units.svg";
 import chevron from "../assets/images/icon-dropdown.svg";
 import search from "../assets/images/icon-search.svg";
+import type React from "react";
+import { useState } from "react";
+
+type Prop = {
+  children: React.ReactNode;
+};
+
+export const Wrapper = ({ children }: Prop) => {
+  return (
+    <>
+      <div className="flex h-full min-h-screen w-full flex-col bg-neutral-900 text-lg">
+        {children}
+      </div>
+    </>
+  );
+};
+
+export const Container = ({ children }: Prop) => {
+  return (
+    <>
+      <div className="flex flex-1 flex-col items-center gap-4 pt-4">
+        {children}
+      </div>
+    </>
+  );
+};
 
 export const Footer = () => {
   return (
     <>
-      <footer>
-        Challenge by
-        <a href="https://www.frontendmentor.io?ref=challenge">
+      <footer className="text-neutral-0 text-center text-xs">
+        Challenge by{" "}
+        <a
+          className="underline"
+          href="https://www.frontendmentor.io?ref=challenge"
+        >
           Frontend Mentor
         </a>
-        . Coded by <a href="https://github.io/jjdavenport">jjdavenport</a>.
+        . Coded by{" "}
+        <a className="underline" href="https://github.io/jjdavenport">
+          jjdavenport
+        </a>
+        .
       </footer>
     </>
   );
@@ -20,8 +53,8 @@ export const Footer = () => {
 export const Header = () => {
   return (
     <>
-      <header>
-        <img src={logo} alt="logo" />
+      <header className="flex w-11/12 justify-between">
+        <img className="object-contain" src={logo} alt="logo" />
         <HeaderDropdown />
       </header>
     </>
@@ -31,7 +64,7 @@ export const Header = () => {
 const HeaderDropdown = () => {
   return (
     <div>
-      <button>
+      <button className="font-DM-Sans text-neutral-0 flex gap-1 rounded-lg bg-neutral-700 px-2 py-1">
         <img src={settings} alt="settings" />
         Units <img src={chevron} alt="chevron" />
       </button>
@@ -40,17 +73,19 @@ const HeaderDropdown = () => {
 };
 
 const HeaderMenu = () => {
-    return (
-        <>
-        <ul></ul>
-        </>
-    )
-}
+  return (
+    <>
+      <ul></ul>
+    </>
+  );
+};
 
 export const Title = () => {
   return (
     <>
-      <h1>How's the sky looking today?</h1>
+      <h1 className="font-DM-Sans text-neutral-0 w-11/12 text-center text-6xl font-bold">
+        How's the sky looking today?
+      </h1>
     </>
   );
 };
@@ -58,75 +93,93 @@ export const Title = () => {
 export const Search = () => {
   return (
     <>
-      <div>
+      <div className="flex w-11/12 gap-4 rounded-lg bg-neutral-700 px-4 py-2">
         <img src={search} alt="search" />
-        <input type="text" placeholder="Search for a place…" />
+        <input
+          className="font-DM-Sans w-full placeholder:text-neutral-200 focus:outline-none"
+          type="text"
+          placeholder="Search for a place…"
+        />
       </div>
     </>
   );
 };
 
 type SearchListProp = {
-    unit: string;
-}
+  unit: string;
+};
 
-const SearchList = ({unit}: SearchListProp) => {
-    return (
-        <>
-        <ul>
-            <button>Switch to {unit}</button>
-            <SearchListItem label="Temperature" buttonOne="Celsius (°C)" buttonTwo="Fahrenheit (°F)"/>
-            <SearchListItem label="Wind Speed" buttonOne="km/h" buttonTwo="mph"/>
-            <SearchListItem label="Precipitation" buttonOne="Millimeters (mm)" buttonTwo="Inches (in)"/>
-        </ul>
-        </>
-    )
-}
+const SearchList = ({ unit }: SearchListProp) => {
+  return (
+    <>
+      <ul>
+        <button>Switch to {unit}</button>
+        <SearchListItem
+          label="Temperature"
+          buttonOne="Celsius (°C)"
+          buttonTwo="Fahrenheit (°F)"
+        />
+        <SearchListItem label="Wind Speed" buttonOne="km/h" buttonTwo="mph" />
+        <SearchListItem
+          label="Precipitation"
+          buttonOne="Millimeters (mm)"
+          buttonTwo="Inches (in)"
+        />
+      </ul>
+    </>
+  );
+};
 
 type SearchListItemsProp = {
   label: string;
   buttonOne: string;
   buttonTwo: string;
-}
+};
 
-const SearchListItem = ({label, buttonOne, buttonTwo}: SearchListItemsProp) => {
-    return (
-        <>
-        <li>
-        <label>{label}</label>
-        <SearchListButton text={buttonOne}/>
-        <SearchListButton text={buttonTwo}/>
-        </li>
-        </>
-    )
-}
-
-type SearchListButtonProp = {
-    text: string;
-}
-
-const SearchListButton = ({text}: SearchListButtonProp) => {
-    return (
-        <>
-        <button>{text}</button>
-        </>
-    )
-}
-
-const SearchButton = () => [
-    return (
-        <>
-        <li>
-            <button></button>
-        </li>
-        </>
-    )
-]
-
-const Button = () => {
+const SearchListItem = ({
+  label,
+  buttonOne,
+  buttonTwo,
+}: SearchListItemsProp) => {
   return (
     <>
-      <button>Search</button>
+      <li>
+        <label>{label}</label>
+        <SearchListButton text={buttonOne} />
+        <SearchListButton text={buttonTwo} />
+      </li>
+    </>
+  );
+};
+
+type SearchListButtonProp = {
+  text: string;
+};
+
+const SearchListButton = ({ text }: SearchListButtonProp) => {
+  return (
+    <>
+      <button>{text}</button>
+    </>
+  );
+};
+
+export const SearchButton = () => {
+  return (
+    <>
+      <li>
+        <button></button>
+      </li>
+    </>
+  );
+};
+
+export const Button = () => {
+  return (
+    <>
+      <button className="font-DM-Sans text-neutral-0 w-11/12 rounded-lg bg-blue-500 p-2">
+        Search
+      </button>
     </>
   );
 };
@@ -142,7 +195,7 @@ export const Main = () => {
 export const List = () => {
   return (
     <>
-      <ul>
+      <ul className="grid w-11/12 grid-cols-2 grid-rows-2 gap-4">
         <ListItem text="Feels like" />
         <ListItem text="Humidity" />
         <ListItem text="Wind" />
@@ -160,7 +213,7 @@ type ListItemProps = {
 const ListItem = ({ text, unit }: ListItemProps) => {
   return (
     <>
-      <li>
+      <li className="font-DM-Sans text-neutral-0 rounded-lg border border-neutral-600 bg-neutral-700 p-4">
         <span>{text}</span>
         <span>{unit}</span>
       </li>
@@ -171,8 +224,10 @@ const ListItem = ({ text, unit }: ListItemProps) => {
 export const DailyList = () => {
   return (
     <>
-      <span>Daily forecast</span>
-      <ul></ul>
+      <section className="w-11/12">
+        <span className="text-neutral-0 font-DM-Sans">Daily forecast</span>
+        <ul className="grid grid-cols-2 grid-rows-3"></ul>
+      </section>
     </>
   );
 };
@@ -203,24 +258,25 @@ const DailyListItem = ({ day, src, alt, high, low }: DailyListItemProps) => {
 export const HourlyList = () => {
   return (
     <>
-      <ul>
-        <div>
-          <span>Hourly forecast</span>
+      <section className="w-11/12 rounded-lg bg-neutral-700 p-4">
+        <div className="flex justify-between">
+          <span className="text-neutral-0 font-DM-Sans">Hourly forecast</span>
           <HourlyDropDown />
         </div>
-      </ul>
+        <ul></ul>
+      </section>
     </>
   );
 };
 
-type HourlyListItem = {
+type HourlyListItemProps = {
   src: string;
   alt: string;
   time: string;
   temp: string;
 };
 
-const HourlyListItem = ({ src, alt, time, temp }: HourlyListItem) => {
+const HourlyListItem = ({ src, alt, time, temp }: HourlyListItemProps) => {
   return (
     <>
       <li>
@@ -235,10 +291,11 @@ const HourlyListItem = ({ src, alt, time, temp }: HourlyListItem) => {
 };
 
 const HourlyDropDown = () => {
+  const [text, setText] = useState("Tuesday");
   return (
     <>
       <div>
-        <button>
+        <button className="font-DM-Sans text-neutral-0 flex gap-2 rounded-lg bg-neutral-600 px-2 py-1">
           {text} <img src={chevron} alt="chevron" />
         </button>
       </div>
